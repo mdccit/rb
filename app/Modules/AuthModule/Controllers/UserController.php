3fb826f434a1f93e3f4f171e2a4982aeb95b536f
Isuru Sandalanka <isuru.sandalanka@qualitapps.com>
2024-08-22 08:46:50 +0530

    #135857 admin account can not delete

--- app/Modules/AuthModule/Controllers/UserController.php
+++ app/Modules/AuthModule/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Extra\CommonResponse;
 use App\Http\Controllers\Controller;
 use App\Modules\AuthModule\Services\UserService;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -20,15 +21,25 @@ class UserController extends Controller
     public function userDelete()
     {
         try{
-           
+            $user =User::connect(config('database.default'))->where('id',auth()->id())->first();
+
+            if($user->user_role_id !=2){
             $this->userService->userDelete();
-            
-            return CommonResponse::getResponse(
-                200,
-                'Successfully Account Deleted',
-                'Successfully Account Deleted',
+                
+                return CommonResponse::getResponse(
+                    200,
+                    'Successfully Account Deleted',
+                    'Successfully Account Deleted',
                 );
 
+            }else{
+                return CommonResponse::getResponse(
+                    422,
+                    'Admin Account Can Not Delete',
+                    'Admin Account Can Not Delete'
+                ); 
+            }
+            
         }catch (\Exception $e){
             return CommonResponse::getResponse(
                 422,
--- app/Modules/AuthModule/Services/UserService.php
+++ app/Modules/AuthModule/Services/UserService.php
@@ -10,18 +10,9 @@ class UserService
 {
     public function userDelete(){
         
-        $user =User::connect(config('database.default'))->where('id',auth()->id())->first();
-
-        if($user->user_role_id !=2){
-
-            User::connect(config('database.default'))->destroy(auth()->id());
-            $user = Auth::user();
-            $user->tokens->each->delete();
-        }
-        
-        
-
-    
+        User::connect(config('database.default'))->destroy(auth()->id());
+        $user = Auth::user();
+        $user->tokens->each->delete();
     }
     
 }
