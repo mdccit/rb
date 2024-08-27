929e75fceb31e2aceece2c9059720ffaff257ad7
Lahiru Kahawatta <lahiru.kahawatta@qualitapps.com>
2024-08-27 08:55:52 +0000

    Updated AccessKey.php

--- app/Http/Middleware/Custom/AccessKey.php
+++ app/Http/Middleware/Custom/AccessKey.php
@@ -18,7 +18,8 @@ class AccessKey
     public function handle(Request $request, Closure $next): Response
     {
         // $accessKey = $request->header("access_key","");
-        $accessKey = $request->server->get('access_key');
+        // $accessKey = $request->server->get('access_key');
+        $accessKey = \Request::header('access_key');
         error_log('access key header : '.$accessKey);
         error_log('access key app : '.config('app.access_key'));
         if(config('app.access_key') != $accessKey){
@@ -29,7 +30,7 @@ class AccessKey
             // );
             return CommonResponse::getResponse(
                 401,
-                'access key header : '.$accessKey,
+                'access key header : '.$accessKey.' | lang : '.$request->header("lang",""),
                 'access key app : '.config('app.access_key')
             );
         }
