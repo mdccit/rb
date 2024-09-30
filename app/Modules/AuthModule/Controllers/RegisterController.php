a21d21e588ee9ddbdcdf4404df83b8f67eee2411
LahiruQualitapps <lahiru.kahawatta@qualitapps.com>
2024-09-30 19:13:44 +0530

    #136624 add password confirmation validation with separate key, #136623 add meaningful error messages

--- app/Modules/AuthModule/Controllers/RegisterController.php
+++ app/Modules/AuthModule/Controllers/RegisterController.php
@@ -52,8 +52,8 @@ class RegisterController extends Controller
 
             return CommonResponse::getResponse(
                 200,
-                'Successfully Updated',
-                'Successfully Updated'
+                'Your account has been successfully updated',
+                'Your account has been successfully updated'
             );
         }catch (\Exception $e){
             return CommonResponse::getResponse(
