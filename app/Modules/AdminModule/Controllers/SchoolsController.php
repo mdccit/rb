08fb97f3fb03eca0e9ccdc01f4cb42782aaa7368
LahiruQualitapps <lahiru.kahawatta@qualitapps.com>
2024-10-04 13:04:54 +0530

    #137123 adding profile,cover and media upload enpoints for business and schools in admin | #137111 fixing bugs on player core values validation

--- app/Modules/AdminModule/Controllers/SchoolsController.php
+++ app/Modules/AdminModule/Controllers/SchoolsController.php
@@ -4,6 +4,7 @@ namespace App\Modules\AdminModule\Controllers;
 
 use App\Extra\CommonResponse;
 use App\Http\Controllers\Controller;
+use App\Models\School;
 use App\Modules\AdminModule\Services\SchoolService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -158,6 +159,154 @@ class SchoolsController extends Controller
                 'Successfully fetched',
                 'Successfully fetched',
                 $responseData
+            );
+        }catch (\Exception $e){
+            return CommonResponse::getResponse(
+                422,
+                $e->getMessage(),
+                'Something went to wrong'
+            );
+        }
+    }
+
+    public function uploadProfilePicture(Request $request,$school_id)
+    {
+        try{
+            $validator = Validator::make($request->all(), [
+                'file.*' => 'required|mimes:jpg,jpeg,png|max:51200',
+            ]);
+            if ($validator->fails())
+            {
+                return CommonResponse::getResponse(
+                    422,
+                    $validator->errors(),
+                    'Input validation failed'
+                );
+            }
+
+            $school = School::connect(config('database.secondary'))
+                ->where('id', $school_id)
+                ->first();
+            if(!$school) {
+                return CommonResponse::getResponse(
+                    401,
+                    'No school associated with this school id',
+                    'No school associated with this school id'
+                );
+            }
+
+            $responseData = $this->schoolService->uploadProfilePicture($request->file('file'),$school_id);
+
+            return CommonResponse::getResponse(
+                200,
+                'Successfully Uploaded',
+                'Successfully Uploaded',
+                $responseData
+            );
+        }catch (\Exception $e){
+            return CommonResponse::getResponse(
+                422,
+                $e->getMessage(),
+                'Something went to wrong'
+            );
+        }
+    }
+
+    public function uploadCoverPicture(Request $request,$school_id)
+    {
+        try{
+            $validator = Validator::make($request->all(), [
+                'file.*' => 'required|mimes:jpg,jpeg,png|max:51200',
+            ]);
+            if ($validator->fails())
+            {
+                return CommonResponse::getResponse(
+                    422,
+                    $validator->errors(),
+                    'Input validation failed'
+                );
+            }
+
+            $school = School::connect(config('database.secondary'))
+                ->where('id', $school_id)
+                ->first();
+            if(!$school) {
+                return CommonResponse::getResponse(
+                    401,
+                    'No school associated with this school id',
+                    'No school associated with this school id'
+                );
+            }
+
+            $responseData = $this->schoolService->uploadCoverPicture($request->file('file'),$school_id);
+
+            return CommonResponse::getResponse(
+                200,
+                'Successfully Uploaded',
+                'Successfully Uploaded',
+                $responseData
+            );
+        }catch (\Exception $e){
+            return CommonResponse::getResponse(
+                422,
+                $e->getMessage(),
+                'Something went to wrong'
+            );
+        }
+    }
+
+    public function uploadMedia(Request $request,$school_id)
+    {
+        try{
+            $validator = Validator::make($request->all(), [
+                'files.*' => 'required|mimes:jpg,jpeg,png,mp4|max:51200',
+            ]);
+            if ($validator->fails())
+            {
+                return CommonResponse::getResponse(
+                    422,
+                    $validator->errors(),
+                    'Input validation failed'
+                );
+            }
+
+            $school = School::connect(config('database.secondary'))
+                ->where('id', $school_id)
+                ->first();
+            if(!$school) {
+                return CommonResponse::getResponse(
+                    401,
+                    'No school associated with this school id',
+                    'No school associated with this school id'
+                );
+            }
+
+            $responseData = $this->schoolService->uploadMedia($request->file('files'),$school_id);
+
+            return CommonResponse::getResponse(
+                200,
+                'Successfully Uploaded',
+                'Successfully Uploaded',
+                $responseData
+            );
+        }catch (\Exception $e){
+            return CommonResponse::getResponse(
+                422,
+                $e->getMessage(),
+                'Something went to wrong'
+            );
+        }
+    }
+
+    public function removeMedia($media_id)
+    {
+        try{
+            $this->schoolService->removeMedia($media_id);
+
+            return CommonResponse::getResponse(
+                200,
+                'Successfully Removed Media',
+                'Successfully Removed Media',
             );
         }catch (\Exception $e){
             return CommonResponse::getResponse(
