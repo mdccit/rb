1c7249c67ab45445456431564ba96147431c0973
Isuru Sandalanka <isuru.sandalanka@qualitapps.com>
2024-10-29 10:03:03 +0530

    #137756 Chat - User role, unread message counts, onlune satus not showing properly

--- app/Modules/UserModule/Services/ConversationService.php
+++ app/Modules/UserModule/Services/ConversationService.php
@@ -49,23 +49,44 @@ class ConversationService
 
         //$per_page_items = array_key_exists("per_page_items",$data)?$data['per_page_items']:0;
 
-        $query = Conversation::connect(config('database.secondary'))
-               
-                ->with('messages')
-                ->with('firstMessageUser')
-                ->with('receivedUser')
-                ->select(
-                    'conversations.id',
-                    'conversations.user1_id',
-                    'conversations.user2_id',
-                    'conversations.is_delete_user1',
-                    'conversations.is_delete_user2',
-                )->where(function ($query) {
-                    $query->where('conversations.user1_id', auth()->id())
-                          ->orWhere('conversations.user2_id', auth()->id());
-                });
+        // $query = Conversation::connect(config('database.secondary'))
+        //          ->with('messages')
+        //         ->with('firstMessageUser')
+        //         ->with('receivedUser')
+        //         ->select(
+        //             'conversations.id',
+        //             'conversations.user1_id',
+        //             'conversations.user2_id',
+        //             'conversations.is_delete_user1',
+        //             'conversations.is_delete_user2',
+        //         )->where(function ($query) {
+        //             $query->where('conversations.user1_id', auth()->id())
+        //                   ->orWhere('conversations.user2_id', auth()->id());
+        //         });
 
-      
+        $query = Conversation::connect(config('database.secondary'))
+        ->with(['messages' => function ($query) {
+            $query->latest()->first(); // Fetch only the latest message for each conversation
+        }])
+        ->with('firstMessageUser')
+        ->with('receivedUser')
+        ->select(
+            'conversations.id',
+            'conversations.user1_id',
+            'conversations.user2_id',
+            'conversations.is_delete_user1',
+            'conversations.is_delete_user2'
+        )
+        ->where(function ($query) {
+            $query->where('conversations.user1_id', auth()->id())
+                  ->orWhere('conversations.user2_id', auth()->id());
+        })
+        ->orderByDesc(
+            ChatMessage::select('created_at') // or 'updated_at' if messages have an updated timestamp
+                ->whereColumn('conversations.id', 'chat_messages.conversation_id')
+                ->latest()
+                ->take(1)
+        );
           //  $dataSet = $query->get();
 
        $dataSet = $query->get()->map(function ($conversation) {
